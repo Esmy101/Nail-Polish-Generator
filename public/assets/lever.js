b8ffe4f4bf744ec14cacbd8429899c9a6890c56d
Esmeralda Romero <esmeralda.r.a@hotmail.com>
2023-01-11 11:44:57 -0500

    Fix: lever to reset after being clicked

---
+++ public/assets/lever.js
@@ -0,0 +1,20 @@
+//Lever Control
+let leverControl = document.getElementById("leverControl");
+
+function reset() {
+  gsap.fromTo(
+    ".lever",
+    { rotate: -30 },
+    { rotate: 30, duration: "1", repeat: 0, ease: "Sine.out" }
+  );
+}
+
+leverControl.addEventListener("click", () => {
+  gsap.fromTo(
+    ".lever",
+    { rotate: 30 },
+    { rotate: -30, duration: "1", repeat: 0, ease: "Sine.out" }
+  );
+
+  setTimeout(reset, 1000);
+});
