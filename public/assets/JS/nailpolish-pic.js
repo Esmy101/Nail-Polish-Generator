071030b3f55c2d3d6b4aad46dfe26a8b1322b621
Esmeralda Romero <esmeralda.r.a@hotmail.com>
2023-01-17 13:15:59 -0500

    Add: nail polish images into a array

---
+++ public/assets/JS/nailpolish-pic.js
@@ -0,0 +1,11 @@
+import Black from "../pictures/nailpolishpics/One-Coat Black.jpg";
+import RoyalBlue from "../pictures/nailpolishpics/Royal_Tea Blue.jpg";
+
+export const NailPolish = [
+  {
+    image: Black,
+  },
+  {
+    image: RoyalBlue,
+  },
+];
---
+++ public/assets/JS/toppolish-pic.js
@@ -0,0 +1,15 @@
+import FlackieHolo from "../pictures/topperpolishpics/Flackie Holo.jpg";
+import LinearHolo from "../pictures/topperpolishpics/Linear Holo.jpg";
+import ScatteredHolo from "../pictures/topperpolishpics/ Scattered Holo.jpg";
+
+export const TopPolish = [
+  {
+    image: FlackieHolo,
+  },
+  {
+    image: LinearHolo,
+  },
+  {
+    image: ScatteredHolo,
+  },
+];
