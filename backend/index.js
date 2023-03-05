dd3a45093275b6b5a931ee9f5900007c8e952280
James <ponce800@hotmail.com>
2023-03-05 15:32:21 -0500

    added some more specific items to the boiler plate for users

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 const express = require('express')
-const app = express()
+require('dotenv').config()
+const routes = require("./routes");
+
+const db = require("./config/connection");
+
+const PORT = process.env.port || 3001;
+const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server for running on port ${PORT}!`);
+  });
+});
---
+++ backend/models/Polish.js
@@ -0,0 +1,21 @@
+const { ObjectID } = require("bson");
+const { Schema, model } = require("mongoose");
+
+const PolishSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    category:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    type:{
+        type: String,
+        required: true,
+        trim: true,
+    }
+
+})
